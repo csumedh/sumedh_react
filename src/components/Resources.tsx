dd53af6f778ef87a8ab3be9dd7af32bff52a9766
csumedh <sumedhc@umd.edu>
2025-04-06 22:40:44 -0400

    -fix "Deployment errors"

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -32,7 +32,7 @@ sudo apt update && sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo tee /usr/share/keyrings/ros-archive-keyring.gpg > /dev/null
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \\
-http://packages.ros.org/ros2/ubuntu \$(lsb_release -cs) main" | \\
+http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main | \\
 sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null`}</pre>
             </li>
             <li><strong>Install ROS 2 Humble Desktop</strong>
